a437ed51b99025d9efc3df64083b8d697de74cc7
NuriaTired <nuriatired@gmail.com>
2019-02-05 21:22:38 +0100

    Primer commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,24 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes (arr){
+    var newMovies = arr.map(function(item){
+        var num1 = 0;
+        var num2 = 0;
+        var newItem = Object.assign({}, item);
+        if(item.duration.includes("h") && !item.duration.includes("min")){
+            newItem.duration.replace("h", "");
+            newItem.duration =parseInt(newItem.duration, 10) *60;
+            console.log(newItem);
+            return newItem;
+        }
+        else if(!item.duration.includes("h") && item.duration.includes("min")){
+            newItem.duration.replace("min", "");
+            newItem.duration =parseInt(newItem.duration, 10);
+            console.log(newItem);
+            return newItem;
+        }
+    }
+}
 
 
 // Get the average of all rates with 2 decimals 
